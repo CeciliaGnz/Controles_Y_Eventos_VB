e860a1fac6ac27d65e0bf967b7076caeed267b01
ceciliaGnz <ceciliasoto200309@gmail.com>
2023-09-25 17:36:05 -0500

    update

--- EjemplosModuloIII/frmRestaurante.Designer.vb
+++ EjemplosModuloIII/frmRestaurante.Designer.vb
@@ -30,7 +30,15 @@ Partial Class frmRestaurante
         Me.Label3 = New System.Windows.Forms.Label()
         Me.cboArroces = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lstMenu = New System.Windows.Forms.ListBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -53,9 +61,9 @@ Partial Class frmRestaurante
         Me.Panel1.Controls.Add(Me.cboArroces)
         Me.Panel1.Controls.Add(Me.Label2)
         Me.Panel1.Location = New System.Drawing.Point(41, 111)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(422, 458)
+        Me.Panel1.Size = New System.Drawing.Size(422, 433)
         Me.Panel1.TabIndex = 1
         '
         'cboCarnes
@@ -63,7 +71,7 @@ Partial Class frmRestaurante
         Me.cboCarnes.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cboCarnes.FormattingEnabled = True
         Me.cboCarnes.Location = New System.Drawing.Point(25, 340)
-        Me.cboCarnes.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cboCarnes.Margin = New System.Windows.Forms.Padding(2)
         Me.cboCarnes.Name = "cboCarnes"
         Me.cboCarnes.Size = New System.Drawing.Size(308, 38)
         Me.cboCarnes.TabIndex = 5
@@ -84,7 +92,7 @@ Partial Class frmRestaurante
         Me.cboMenestras.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cboMenestras.FormattingEnabled = True
         Me.cboMenestras.Location = New System.Drawing.Point(25, 201)
-        Me.cboMenestras.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cboMenestras.Margin = New System.Windows.Forms.Padding(2)
         Me.cboMenestras.Name = "cboMenestras"
         Me.cboMenestras.Size = New System.Drawing.Size(308, 38)
         Me.cboMenestras.TabIndex = 3
@@ -106,7 +114,7 @@ Partial Class frmRestaurante
         Me.cboArroces.FormattingEnabled = True
         Me.cboArroces.Items.AddRange(New Object() {"Arroz Blanco: 0.90", "Arroz con Guandu:1.75", "Arroz con Vegetales:1.50", "Arroz con Curry:1.80"})
         Me.cboArroces.Location = New System.Drawing.Point(25, 72)
-        Me.cboArroces.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.cboArroces.Margin = New System.Windows.Forms.Padding(2)
         Me.cboArroces.Name = "cboArroces"
         Me.cboArroces.Size = New System.Drawing.Size(308, 38)
         Me.cboArroces.TabIndex = 1
@@ -122,18 +130,96 @@ Partial Class frmRestaurante
         Me.Label2.TabIndex = 0
         Me.Label2.Text = "ARROCES" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(488, 111)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(307, 31)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Su menú seleccionado"
+        '
+        'lstMenu
+        '
+        Me.lstMenu.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstMenu.FormattingEnabled = True
+        Me.lstMenu.ItemHeight = 25
+        Me.lstMenu.Location = New System.Drawing.Point(494, 171)
+        Me.lstMenu.Name = "lstMenu"
+        Me.lstMenu.Size = New System.Drawing.Size(388, 229)
+        Me.lstMenu.TabIndex = 3
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(489, 420)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(467, 25)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "Doble clic al elementro que desea eleminar"
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.txtTotal)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Location = New System.Drawing.Point(499, 474)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(383, 78)
+        Me.Panel2.TabIndex = 5
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(73, 38)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(245, 32)
+        Me.txtTotal.TabIndex = 7
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(173, 10)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(49, 20)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Total"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(136, 560)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(212, 35)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Borrar Menú"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'frmRestaurante
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1145, 632)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lstMenu)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmRestaurante"
         Me.Text = "frmRestaurante"
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -147,4 +233,11 @@ Partial Class frmRestaurante
     Friend WithEvents Label3 As Label
     Friend WithEvents cboArroces As ComboBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lstMenu As ListBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button1 As Button
 End Class
